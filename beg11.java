02ea018916cfe4decf4688534e08e4321d37492c
Nafeela <nnafeelabanu@gmail.com>
2019-07-26 21:51:59 +0530

    Add files via upload

---
+++ beg11.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+public class beg11{
+public static void main(String[] args) throws java.lang.Exception{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int a=s.nextInt();
+int b=(int)Math.pow(n,a);
+System.out.println(b);
+}
+}
