5163c17bbfee8e6c38f12ef2a1e57f1f04e3a0d3
Nafeela <nnafeelabanu@gmail.com>
2019-07-26 22:16:12 +0530

    Add files via upload

---
+++ pla1.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+public class pla1{
+public static void main(String[] args) throws java.lang.Exception{
+Scanner s=new Scanner(System.in);
+String str=s.nextLine();
+StringBuilder sb = new StringBuilder(str);
+System.out.println(sb.reverse().toString());
+}
+}
