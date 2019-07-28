33cc4f91d54e84844e676c92a480cb6a91384596
Nafeela <nnafeelabanu@gmail.com>
2019-07-28 21:06:52 +0530

    Add files via upload

---
+++ hun1.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+public class hun1{
+public static void main(String[] args) throws java.lang.Exception{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+boolean ans;
+List<Integer> list=new ArrayList<Integer>();
+int a[]=new int[n];
+for(int i=0;i<n;i++)
+	a[i]=s.nextInt();
+for(int i=0;i<n;i++){
+	for(int j=i+1;j<n;j++){
+		if(a[i]==a[j]){
+			ans=list.contains(a[i]);
+			if(!ans)
+			list.add(a[i]);
+		}
+	}
+}
+Collections.sort(list);
+for(int i=0;i<list.size();i++){
+    System.out.print(list.get(i)+" ");
+} 
+}
+}
