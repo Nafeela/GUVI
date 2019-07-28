47c5410cbb1056a627c7fea1d489e73847372cf7
Nafeela <nnafeelabanu@gmail.com>
2019-07-28 14:39:45 +0530

    Add files via upload

---
+++ pro2.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+public class pro2{
+public static void main(String[] args) throws java.lang.Exception{
+Scanner s=new Scanner(System.in);
+String n=s.nextLine();
+int k=s.nextInt();
+//String s=String.valueOf(n);
+String ans=removeKdigits(n,k);
+System.out.println(ans);
+}
+static String removeKdigits(String num, int k) {
+    if(num.length()==k)
+        return "0";
+ 
+    StringBuilder sb = new StringBuilder(num);
+    for(int j=0; j<k; j++){
+        int i=0;
+        while(i<sb.length()-1&&sb.charAt(i)<=sb.charAt(i+1)){
+            i++;
+        }
+        sb.delete(i, i+1);
+    }
+ 
+    //remove leading 0's        
+    while (sb.length() > 1 && sb.charAt(0)=='0')
+        sb.delete(0,1);
+ 
+    if(sb.length()==0){
+        return "0";
+    }
+ 
+    return sb.toString();
+}
+}
