60d793986e33115d6b71c087a9ac047763bd69c5
Nafeela <nnafeelabanu@gmail.com>
2019-07-30 21:47:58 +0530

    Add files via upload

---
+++ beg14.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+public class beg14{
+public static void main(String[] args) throws java.lang.Exception{
+int r,sum=0,temp;    
+  Scanner s=new Scanner(System.in);
+int n1=s.nextInt();
+int n2=s.nextInt();
+for(int i=n1+1;i<n2;i++)
+	if(i%2!=0)
+		System.out.print(i+" ");
+}
+}
